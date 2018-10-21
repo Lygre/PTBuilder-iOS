a36da754785e7e107249f652c2fca5171a13d0c6
Hugh Broome <hughdylanbroome@gmail.com>
2018-10-21 09:55:31 -0400

    Init

--- PTBuilder iOS/FirstViewController.swift
+++ PTBuilder iOS/FirstViewController.swift
@@ -10,11 +10,102 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+	@IBOutlet weak var monSearchField: UITextField!
+	
+	@IBOutlet weak var tableView: UITableView!
+	
+	@IBOutlet weak var monAddedName: UILabel!
+	
+	@IBOutlet weak var memberName: UILabel!
+	
+	
+	
+	@objc dynamic var team = [Pokemon]()
+	
+	@objc dynamic var team2test = Team()
+	
+//	@objc dynamic var teamWeaknessTableBind: [String: [String: NSImage]] = [:]
+	
+	@objc dynamic var teamCoverageTableBind: [String: [String: Bool]] = [:]
+	
+	@objc dynamic var teamAttributeTableBind: [String: Bool] = [:]
+	
+//	@objc dynamic var colorTable: [String: NSColor] = ["Weak": NSColor.systemRed,
+//													   "Resist": NSColor.systemGreen,
+//													   "Neutral": NSColor.systemGray]
+//
+	@objc dynamic var suggestedMonTableBind = [Pokemon]()
+	
+	@objc dynamic var itemList = [Item]()
+	
+	@objc dynamic var learnsetMoves = [Move]()
+	
+	@objc dynamic var movesToSet = [String: Move]()
+	
+	@objc dynamic var natureList = [String]()
+	
+	@objc dynamic var levelArray = [Int]()
+	
+//	@objc dynamic var selectedMonImage = NSImage()
+	
+	@objc dynamic var evHPBind = Int()
+	@objc dynamic var evATKBind = Int()
+	@objc dynamic var evDEFBind = Int()
+	@objc dynamic var evSPABind = Int()
+	@objc dynamic var evSPDBind = Int()
+	@objc dynamic var evSPEBind = Int()
+	
+	@objc dynamic var evHPMaxValueBind = Double()
+	@objc dynamic var evATKMaxValueBind = Double()
+	@objc dynamic var evDEFMaxValueBind = Double()
+	@objc dynamic var evSPAMaxValueBind = Double()
+	@objc dynamic var evSPDMaxValueBind = Double()
+	@objc dynamic var evSPEMaxValueBind = Double()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		Dex.initializeDex()
+		Dex.defineTypeMatchups()
+		MoveDex.initializeMoveDex()
+		ItemDex.initializeItemDex()
+		for item in ItemDex.itemDexArray {
+			itemList.append(item)
+		}
+		for (nature, _) in Dex.natureList {
+			natureList.append(nature)
+		}
+		natureList.sort()
+		repeat {
+			levelArray.append(levelArray.count + 1)
+		} while levelArray.count < 100
+	
 	}
 
-
+	@IBAction func searchMonForAdd(_ sender: Any) {
+		
+		let monToAdd = Dex.searchDex(searchParam: monSearchField.text!)[0]
+		//set default level at 100
+		print(monSearchField.text!)
+		monToAdd.level = 100
+		//set default IVs all to 31
+		for (stat, _) in monToAdd.iVs {
+			monToAdd.iVs[stat] = 31
+		}
+		monToAdd.nature = "mild"
+		//calc actual stats
+		monToAdd.actualStats = Pokemon.calcStats(pokemon: monToAdd)
+		monToAdd.virtualStats = Pokemon.calcVirtualStats(pokemon: monToAdd)
+		//add pokemon object to objc team variable (bound to array controller)
+		self.team.append(monToAdd)
+		if team2test.members.isEmpty {
+			team2test = Team(members: team)
+		} else {
+			team2test.addMember(monToAdd)
+		}
+		
+		monAddedName.text = monToAdd.species
+	}
+	
 }
 
