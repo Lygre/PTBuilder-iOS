af679f9c19d62b20da4ef8f427fa23e67c25d321
Hugh Broome <hughdylanbroome@gmail.com>
2018-11-16 18:16:50 -0500

    Initial Commit

---
+++ PTB Universal/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PTB Universal
+//
+//  Created by Hugh Broome on 11/16/18.
+//  Copyright © 2018 Hugh Broome. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
